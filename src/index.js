9352d1086b23ee305661bb77b85104cf86f8d055
Jason Li <li.jasontse@gmail.com>
2021-04-28 05:14:13 -0700

    WAdd csv files for d3 to file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+// import * as d3 from 'd3';
 
-import * as d3 from 'd3';
+// d3.csv("base_weapon_stats.csv", d => {
+//   console.log(d)
+// });
+
+// d3.csv("attachment_stats - Sheet1.csv", d => {
+//   console.log(d)
+// });
 
 
+
+
+
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.75,
+}
+
+const observer = new IntersectionObserver(callback, options);
 
