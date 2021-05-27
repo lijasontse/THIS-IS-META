b04e10e0fe0d12b84f33a3f686dd9fc15fd3cd97
Jason Li <li.jasontse@gmail.com>
2021-05-27 07:07:59 -0700

    Add meta attachment dropdown and hover effects over bars

--- dist/bundle.js
+++ dist/bundle.js
@@ -30686,179 +30686,21 @@ var __webpack_exports__ = {};
   \**********************/
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var d3__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! d3 */ "./node_modules/d3/index.js");
- // var joinedData;
-// d3.csv("joined_data_u.csv", d => {
-//   return {
-//     game: d["Game"],
-//     name: d["Name"],
-//     weaponId: d["Weapon ID"],
-//     classId: d["Class ID"],
-//     type: d["Type"],
-//     accuracy: d["Accuracy"],
-//     damage: d["Damage"],
-//     range: d["Range"],
-//     fireRate: d["Fire Rate"],
-//     mobility: d["Mobility"],
-//     control: d["Control"]
-//   };
-// }).then(data => {
-//     joinedData = data;
-//     var baseData = [];
-//     var metaData = [];
-//     // createBarPlot(joinedData[0], 0)
-//     // appendNavs(0);
-//     // appendLinkTags(0);
-//     for (var i = 0; i < joinedData.length; i++) {
-//       if (joinedData[i]["type"] === "Base") {
-//         baseData.push(joinedData[i]);
-//       } else {
-//         metaData.push(joinedData[i]);
-//       }
-//     }
-//     // for (let i = 1; i < baseData.length; i++) {
-//     //   createBarPlot(baseData[i], [i])
-//     //   appendNavs(i);
-//     //   appendLinkTags(i);
-//     // }
-//     for (let i = 0; i < metaData.length; i++) {
-//       createBarPlot(metaData[i], [i])
-//       appendNavs(i);
-//       appendLinkTags(i);
-//     }
-// });
-// function change(value) {
-//   if (value === "base") {
-//     update(baseData);
-//   } else if (value === 'meta') {
-//     update(metaData);
-//   } else {
-//     update(joinedData);
-//   }
-// }
-// const createBarPlot = (joinedData, idx) => {
-//   var margin = {top: 5, right: 40, bottom: 5, left: 90};
-//   var width = 700 - margin.left - margin.right;
-//   var height = 500 - margin.top - margin.bottom;
-//   var data = Object.values(joinedData).slice(5);
-//   let numColumns = 6;
-//   var x_axisLength = width;
-//   var y_axisLength = height;
-//   var targetSVG = "slide-svg-" + idx;
-//   var xChart = d3
-//     .scaleLinear()
-//     .range([0, width])
-//     .domain([0, numColumns]);
-//   var yChart = d3
-//     .scaleLinear()
-//     .range([height, 0])
-//     .domain([0, 300]);
-//   var svg = d3
-//     .select("#meta-vis")
-//     .append("svg")
-//       .attr("class", `${targetSVG} hidden`)
-//       .attr("viewBox", `0 0 650 700`)
-//       .attr("preserveAspectRatio", "xMinYMin meet")
-//     .append("g")
-//       .attr("transform",
-//             "translate(" + margin.left + ", " + (margin.top + 20) + ")");
-//   var xAxis = d3
-//     .axisBottom(xChart)
-//     .tickSize(0)
-//     .tickFormat(function (d) {
-//       return Object.keys(joinedData).slice(5)[d];
-//   });
-//   svg
-//     .append("g")
-//     .attr("class", `${targetSVG}-x-axis x-axis`)
-//     .attr("transform", "translate(0," + height + ")")
-//     .transition()
-//     .duration(1000)
-//     .call(xAxis);
-//   svg.selectAll("x-axis, text")
-//     .attr("transform", "translate(10, 40)rotate(-42)")
-//     .style("text-anchor", "start");
-//   var yAxis = d3
-//     .axisLeft(yChart)
-//     .ticks(5);
-//     svg
-//       .append("g")
-//       .attr("class", `${targetSVG}-y-axis y-axis`)
-//       .attr("transform", "translate(" + (margin.right - 50) + ",0)")
-//       // .style('opacity', '0%')
-//       .call(yAxis);
-//   svg
-//   .append("text")
-//   .attr("transform", "rotate(-90)")
-//   .attr("class", "y-axis-label")
-//   .attr("y", -80)
-//   .attr("x", 0 - height / 2)
-//   .attr("dy", "1em")
-//   .style("text-anchor", "middle")
-//   .text("Base stats of the current meta weapons in Call of Duty: Warzone")
-//   svg
-//     .append("text")
-//     .attr("class", "source-text")
-//     .attr("transform",
-//     "translate(0, " + (height + margin.top + 70) + ")")
-//     .style("text-anchor", "left")
-//     .text("Source: Call of Duty Warzone");
-//   const update = (data, idx) => {
-//     let numColumns = 6;
-//     xChart.domain([0, numColumns]);
-//     yChart.domain([0, 300]);
-//     let targetSlideRect = "slide-svg-" + idx + "-rect";
-//     let barWidth = x_axisLength / numColumns;
-//     let updatedRects = svg
-//     .selectAll('rect')
-//     .remove()
-//     .exit()
-//     .data(data)
-//     debugger
-//     updatedRects.enter()
-//       .append("rect")
-//       .attr("class", `${targetSlideRect}`)
-//       .attr("x", function (d, i) {
-//         return i * (barWidth) + 12;
-//       })
-//       .attr("y", function (d) {
-//         return yChart(d)
-//       })
-//       .attr("height", function (d) {
-//         return height - yChart(d)
-//       })
-//       .attr("width", barWidth - 45)
-//       .attr("fill", function (d) {
-//         if (d.type === "base") {
-//           return "rgb(251,180,174)";
-//         } else {
-//           return "rgb(179,205,227)";
-//         }
-//       })
-//     svg.select(`.slide-svg-${idx}-y-axis`).call(yAxis)
-//     svg.select(`.slide-svg-${idx}-x-axis`).call(xAxis)
-//       .selectAll("text")
-//       .style("text-anchor", "end")
-//       .attr("transform", function (d) {
-//         return "rotate(-65)";
-//       });
-//   }
-//   update(joinedData);
-// }
 
 var weaponData;
-d3__WEBPACK_IMPORTED_MODULE_0__.csv("base_weapon_stats.csv", function (d) {
+d3__WEBPACK_IMPORTED_MODULE_0__.csv("meta_weapon.csv", function (d) {
   return {
     game: d["Game"],
     name: d["Name"],
     type: d["Type"],
     weaponId: d["Weapon ID"],
     classId: d["Class ID"],
-    accuracy: d["Accuracy"],
-    damage: d["Damage"],
-    range: d["Range"],
-    fireRate: d["Fire Rate"],
-    mobility: d["Mobility"],
-    control: d["Control"]
+    accuracy: +d["Accuracy"],
+    damage: +d["Damage"],
+    range: +d["Range"],
+    fireRate: +d["Fire Rate"],
+    mobility: +d["Mobility"],
+    control: +d["Control"]
   };
 }).then(function (data) {
   weaponData = data;
@@ -30895,7 +30737,8 @@ var createBarPlot = function createBarPlot(weaponData, idx) {
   var svg = d3__WEBPACK_IMPORTED_MODULE_0__.select("#meta-vis").append("svg").attr("class", "".concat(targetSVG, " hidden")).attr("viewBox", "0 0 650 700").attr("preserveAspectRatio", "xMinYMin meet").append("g").attr("transform", "translate(" + margin.left + ", " + (margin.top + 20) + ")");
   var xAxis = d3__WEBPACK_IMPORTED_MODULE_0__.axisBottom(xScale).tickSize(0).tickFormat(function (d) {
     return Object.keys(weaponData).slice(5)[d];
-  });
+  }); // console.log(Object.keys(weaponData).slice(5))
+
   svg.append("g").attr("class", "".concat(targetSVG, "-x-axis x-axis")).attr("transform", "translate(0," + height + ")").transition().duration(1000).call(xAxis);
   svg.selectAll("x-axis, text").attr("transform", "translate(10, 40)rotate(-42)").style("text-anchor", "start");
   var yAxis = d3__WEBPACK_IMPORTED_MODULE_0__.axisLeft(yScale).ticks(5);
@@ -30910,55 +30753,16 @@ var createBarPlot = function createBarPlot(weaponData, idx) {
   }).attr("width", x_axisLength / numColumns - 45).attr("height", function (d) {
     return height - yScale(d);
   }).transition().duration(1500);
-};
-
-var activeDropdown = {};
-document.getElementById('muzzle-dropdown').addEventListener('click', showDropdown);
-document.getElementById('barrel-dropdown').addEventListener('click', showDropdown);
-document.getElementById('laser-dropdown').addEventListener('click', showDropdown);
-document.getElementById('optic-dropdown').addEventListener('click', showDropdown);
-document.getElementById('stock-dropdown').addEventListener('click', showDropdown);
-document.getElementById('underbarrel-dropdown').addEventListener('click', showDropdown);
-document.getElementById('ammo-dropdown').addEventListener('click', showDropdown);
-document.getElementById('reargrip-dropdown').addEventListener('click', showDropdown);
-
-function showDropdown(event) {
-  if (activeDropdown.id && activeDropdown.id !== event.target.id) {
-    activeDropdown.element.classList.remove('active');
-  } //checking if a list element was clicked, changing the inner button value
-
-
-  if (event.target.tagName === 'LI') {
-    activeDropdown.button.innerHTML = event.target.innerHTML;
-
-    for (var i = 0; i < event.target.parentNode.children.length; i++) {
-      if (event.target.parentNode.children[i].classList.contains('check')) {
-        event.target.parentNode.children[i].classList.remove('check');
-      }
-    } //timeout here so the check is only visible after opening the dropdown again
-
-
-    window.setTimeout(function () {
-      event.target.classList.add('check');
-    }, 500);
-  }
-
-  for (var i = 0; i < this.children.length; i++) {
-    if (this.children[i].classList.contains('dropdown-selection')) {
-      activeDropdown.id = this.id;
-      activeDropdown.element = this.children[i];
-      this.children[i].classList.add('active');
-    } //adding the dropdown-button to our object
-    else if (this.children[i].classList.contains('dropdown-button')) {
-        activeDropdown.button = this.children[i];
-      }
-  }
-}
-
-window.onclick = function (event) {
-  if (!event.target.classList.contains('dropdown-button')) {
-    activeDropdown.element.classList.remove('active');
-  }
+  var currentSVG = d3__WEBPACK_IMPORTED_MODULE_0__.select(".slide-svg-".concat(idx));
+  var stats = ['Accuracy', 'Damage', 'Range', 'FireRate', 'Mobility', 'Control'];
+  var tooltip = d3__WEBPACK_IMPORTED_MODULE_0__.select('body').append('div').attr("class", "rect-hover-".concat(idx)).style("position", "absolute").style("font-size", "12px").style("z-index", "10").style("visibility", 'hidden');
+  currentSVG.selectAll("rect").on("mouseover", function (d) {
+    return tooltip.style("visibility", "visible");
+  }).on("mousemove", function (d, i) {
+    return tooltip.style("top", event.pageY - 40 + "px").style("left", event.pageX - 40 + "px").style("background-color", "#687864").style("padding", "3px 9px").style("border-radius", "3px").text("".concat(i));
+  }).on("mouseout", function () {
+    return tooltip.style("visibility", "hidden");
+  });
 };
 
 var appendNavs = function appendNavs(idx) {
@@ -30983,7 +30787,7 @@ var appendLinkTags = function appendLinkTags(idx) {
 var createObserver = function createObserver(containers) {
   var options = {
     root: null,
-    threshold: 0.8,
+    threshold: 0.7,
     rootMargin: '0px'
   };
 
@@ -31056,6 +30860,56 @@ var renderSlide = function renderSlide(options, slide, idx) {
 
   var observer = new IntersectionObserver(handleSlide, options);
   observer.observe(slide);
+};
+
+var activeDropdown = {};
+document.getElementById('attach1-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach2-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach3-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach4-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach5-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach6-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach7-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach8-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach9-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach10-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach11-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach12-dropdown').addEventListener('click', showDropdown);
+
+function showDropdown(event) {
+  if (activeDropdown.id && activeDropdown.id !== event.target.id) {
+    activeDropdown.element.classList.remove('active');
+  } //checking if a list element was clicked, changing the inner button value
+  // if (event.target.tagName === 'LI') {
+  //   activeDropdown.button.innerHTML = event.target.innerHTML;
+  //   for (var i = 0; i < event.target.parentNode.children.length; i++) {
+  //     if (event.target.parentNode.children[i].classList.contains('check')) {
+  //       event.target.parentNode.children[i].classList.remove('check');
+  //     }
+  //   }
+  //   //timeout here so the check is only visible after opening the dropdown again
+  //   window.setTimeout(function () {
+  //     event.target.classList.add('check');
+  //   }, 300)
+  // }
+
+
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].classList.contains('dropdown-selection')) {
+      activeDropdown.id = this.id;
+      activeDropdown.element = this.children[i];
+      this.children[i].classList.add('active');
+    } //adding the dropdown-button to our object
+    else if (this.children[i].classList.contains('dropdown-button')) {
+        activeDropdown.button = this.children[i];
+      }
+  }
+}
+
+window.onclick = function (event) {
+  if (!event.target.classList.contains('dropdown-button')) {
+    activeDropdown.element.classList.remove('active');
+  }
 };
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,212 +1,23 @@
 import * as d3 from 'd3';
 
-// var joinedData;
-
-// d3.csv("joined_data_u.csv", d => {
-//   return {
-//     game: d["Game"],
-//     name: d["Name"],
-//     weaponId: d["Weapon ID"],
-//     classId: d["Class ID"],
-//     type: d["Type"],
-//     accuracy: d["Accuracy"],
-//     damage: d["Damage"],
-//     range: d["Range"],
-//     fireRate: d["Fire Rate"],
-//     mobility: d["Mobility"],
-//     control: d["Control"]
-//   };
-// }).then(data => {
-//     joinedData = data;
-
-
-//     var baseData = [];
-//     var metaData = [];
-//     // createBarPlot(joinedData[0], 0)
-//     // appendNavs(0);
-//     // appendLinkTags(0);
-    
-
-//     for (var i = 0; i < joinedData.length; i++) {
-//       if (joinedData[i]["type"] === "Base") {
-//         baseData.push(joinedData[i]);
-//       } else {
-//         metaData.push(joinedData[i]);
-//       }
-//     }
-
-//     // for (let i = 1; i < baseData.length; i++) {
-//     //   createBarPlot(baseData[i], [i])
-//     //   appendNavs(i);
-//     //   appendLinkTags(i);
-//     // }
-
-//     for (let i = 0; i < metaData.length; i++) {
-//       createBarPlot(metaData[i], [i])
-//       appendNavs(i);
-//       appendLinkTags(i);
-//     }
-
-// });
-
-
-// function change(value) {
-//   if (value === "base") {
-//     update(baseData);
-//   } else if (value === 'meta') {
-//     update(metaData);
-//   } else {
-//     update(joinedData);
-//   }
-// }
-
-
-// const createBarPlot = (joinedData, idx) => {
-//   var margin = {top: 5, right: 40, bottom: 5, left: 90};
-//   var width = 700 - margin.left - margin.right;
-//   var height = 500 - margin.top - margin.bottom;
-//   var data = Object.values(joinedData).slice(5);
-//   let numColumns = 6;
-
-//   var x_axisLength = width;
-//   var y_axisLength = height;
-//   var targetSVG = "slide-svg-" + idx;
-
-//   var xChart = d3
-//     .scaleLinear()
-//     .range([0, width])
-//     .domain([0, numColumns]);
-  
-//   var yChart = d3
-//     .scaleLinear()
-//     .range([height, 0])
-//     .domain([0, 300]);
-  
-//   var svg = d3
-//     .select("#meta-vis")
-//     .append("svg")
-//       .attr("class", `${targetSVG} hidden`)
-//       .attr("viewBox", `0 0 650 700`)
-//       .attr("preserveAspectRatio", "xMinYMin meet")
-//     .append("g")
-//       .attr("transform",
-//             "translate(" + margin.left + ", " + (margin.top + 20) + ")");
-  
-//   var xAxis = d3
-//     .axisBottom(xChart)
-//     .tickSize(0)
-//     .tickFormat(function (d) {
-//       return Object.keys(joinedData).slice(5)[d];
-//   });
-  
-//   svg
-//     .append("g")
-//     .attr("class", `${targetSVG}-x-axis x-axis`)
-//     .attr("transform", "translate(0," + height + ")")
-//     .transition()
-//     .duration(1000)
-//     .call(xAxis);
-
-//   svg.selectAll("x-axis, text")
-//     .attr("transform", "translate(10, 40)rotate(-42)")
-//     .style("text-anchor", "start");
-  
-//   var yAxis = d3
-//     .axisLeft(yChart)
-//     .ticks(5);
-
-//     svg
-//       .append("g")
-//       .attr("class", `${targetSVG}-y-axis y-axis`)
-//       .attr("transform", "translate(" + (margin.right - 50) + ",0)")
-//       // .style('opacity', '0%')
-//       .call(yAxis);
-
-//   svg
-//   .append("text")
-//   .attr("transform", "rotate(-90)")
-//   .attr("class", "y-axis-label")
-//   .attr("y", -80)
-//   .attr("x", 0 - height / 2)
-//   .attr("dy", "1em")
-//   .style("text-anchor", "middle")
-//   .text("Base stats of the current meta weapons in Call of Duty: Warzone")
-
-//   svg
-//     .append("text")
-//     .attr("class", "source-text")
-//     .attr("transform",
-//     "translate(0, " + (height + margin.top + 70) + ")")
-//     .style("text-anchor", "left")
-//     .text("Source: Call of Duty Warzone");
-
-//   const update = (data, idx) => {
-//     let numColumns = 6;
-//     xChart.domain([0, numColumns]);
-//     yChart.domain([0, 300]);
-//     let targetSlideRect = "slide-svg-" + idx + "-rect";
-
-//     let barWidth = x_axisLength / numColumns;
-
-//     let updatedRects = svg
-//     .selectAll('rect')
-//     .remove()
-//     .exit()
-//     .data(data)
-//     debugger
-//     updatedRects.enter()
-//       .append("rect")
-//       .attr("class", `${targetSlideRect}`)
-//       .attr("x", function (d, i) {
-//         return i * (barWidth) + 12;
-//       })
-//       .attr("y", function (d) {
-//         return yChart(d)
-//       })
-
-//       .attr("height", function (d) {
-//         return height - yChart(d)
-//       })
-//       .attr("width", barWidth - 45)
-//       .attr("fill", function (d) {
-//         if (d.type === "base") {
-//           return "rgb(251,180,174)";
-//         } else {
-//           return "rgb(179,205,227)";
-//         }
-//       })
-
-
-//     svg.select(`.slide-svg-${idx}-y-axis`).call(yAxis)
-//     svg.select(`.slide-svg-${idx}-x-axis`).call(xAxis)
-//       .selectAll("text")
-//       .style("text-anchor", "end")
-//       .attr("transform", function (d) {
-//         return "rotate(-65)";
-//       });
-//   }
-//   update(joinedData);
-// }
-
 let weaponData;
 
-d3.csv("base_weapon_stats.csv", d => {
+d3.csv("meta_weapon.csv", d => {
   return {
     game: d["Game"],
     name: d["Name"],
     type: d["Type"],
     weaponId: d["Weapon ID"],
     classId: d["Class ID"],
-    accuracy: d["Accuracy"],
-    damage: d["Damage"],
-    range: d["Range"],
-    fireRate: d["Fire Rate"],
-    mobility: d["Mobility"],
-    control: d["Control"]
+    accuracy: +d["Accuracy"],
+    damage: +d["Damage"],
+    range: +d["Range"],
+    fireRate: +d["Fire Rate"],
+    mobility: +d["Mobility"],
+    control: +d["Control"]
   };
 }).then(data => {
     weaponData = data;
-
     createBarPlot(weaponData[0], 0);
     appendNavs(0);
     appendLinkTags(0);
@@ -217,6 +28,7 @@ d3.csv("base_weapon_stats.csv", d => {
       appendLinkTags(i);
     };
   });
+
 
 const createBarPlot = (weaponData, idx) => {
   let margin = {top: 5, right: 40, bottom: 5, left: 90};
@@ -256,6 +68,8 @@ const createBarPlot = (weaponData, idx) => {
     .tickFormat(function (d) {
       return Object.keys(weaponData).slice(5)[d];
     });
+    // console.log(Object.keys(weaponData).slice(5))
+
 
     svg
       .append("g")
@@ -318,54 +132,36 @@ const createBarPlot = (weaponData, idx) => {
   })
   .transition()
   .duration(1500);
-}
 
+  let currentSVG = d3.select(`.slide-svg-${idx}`);
+  const stats = ['Accuracy', 'Damage', 'Range', 'FireRate', 'Mobility', 'Control']
 
-var activeDropdown = {};
-document.getElementById('muzzle-dropdown').addEventListener('click', showDropdown);
-document.getElementById('barrel-dropdown').addEventListener('click', showDropdown);
-document.getElementById('laser-dropdown').addEventListener('click', showDropdown);
-document.getElementById('optic-dropdown').addEventListener('click', showDropdown);
-document.getElementById('stock-dropdown').addEventListener('click', showDropdown);
-document.getElementById('underbarrel-dropdown').addEventListener('click', showDropdown);
-document.getElementById('ammo-dropdown').addEventListener('click', showDropdown);
-document.getElementById('reargrip-dropdown').addEventListener('click', showDropdown);
+  let tooltip = d3
+    .select('body')
+    .append('div')
+    .attr("class", `rect-hover-${idx}`)
+    .style("position", "absolute")
+    .style("font-size", "12px")
+    .style("z-index", "10")
+    .style("visibility", 'hidden');
 
-
-function showDropdown(event) {
-  if (activeDropdown.id && activeDropdown.id !== event.target.id) {
-    activeDropdown.element.classList.remove('active');
-  }
-  //checking if a list element was clicked, changing the inner button value
-  if (event.target.tagName === 'LI') {
-    activeDropdown.button.innerHTML = event.target.innerHTML;
-    for (var i = 0; i < event.target.parentNode.children.length; i++) {
-      if (event.target.parentNode.children[i].classList.contains('check')) {
-        event.target.parentNode.children[i].classList.remove('check');
-      }
-    }
-    //timeout here so the check is only visible after opening the dropdown again
-    window.setTimeout(function () {
-      event.target.classList.add('check');
-    }, 500)
-  }
-  for (var i = 0; i < this.children.length; i++) {
-    if (this.children[i].classList.contains('dropdown-selection')) {
-      activeDropdown.id = this.id;
-      activeDropdown.element = this.children[i];
-      this.children[i].classList.add('active');
-    }
-    //adding the dropdown-button to our object
-    else if (this.children[i].classList.contains('dropdown-button')) {
-      activeDropdown.button = this.children[i];
-    }
-  }
-}
-
-window.onclick = function (event) {
-  if (!event.target.classList.contains('dropdown-button')) {
-    activeDropdown.element.classList.remove('active');
-  }
+  currentSVG
+    .selectAll("rect")
+    .on("mouseover", function (d) {
+      return tooltip.style("visibility", "visible");
+    })
+    .on("mousemove", function (d, i) {
+      return tooltip
+        .style("top", event.pageY - 40 + "px")
+        .style("left", event.pageX - 40 + "px")
+        .style("background-color", "#687864")
+        .style("padding", "3px 9px")
+        .style("border-radius", "3px")
+        .text(`${i}`);
+      })
+      .on("mouseout", function () {
+      return tooltip.style("visibility", "hidden");
+    });
 }
 
 const appendNavs = (idx) => {
@@ -392,7 +188,7 @@ const appendLinkTags = (idx) => {
 const createObserver = containers => {
   let options = {
     root: null,
-    threshold: 0.8,
+    threshold: 0.7,
     rootMargin: '0px'
   }
   for (let i = 0; i < containers.length - 1; i++) {
@@ -416,6 +212,7 @@ const renderSlide = (options, slide, idx) => {
   const handleSlide = (entries, obs) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
+
         document.querySelector(`.slide-svg-${idx}`).classList.remove('hidden');
   
         if (document.querySelector(`.slide-svg-${idx - 1}`)) {
@@ -479,3 +276,57 @@ const renderSlide = (options, slide, idx) => {
   let observer = new IntersectionObserver(handleSlide, options);
   observer.observe(slide);
 }
+
+
+var activeDropdown = {};
+document.getElementById('attach1-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach2-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach3-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach4-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach5-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach6-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach7-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach8-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach9-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach10-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach11-dropdown').addEventListener('click', showDropdown);
+document.getElementById('attach12-dropdown').addEventListener('click', showDropdown);
+
+
+function showDropdown(event) {
+  if (activeDropdown.id && activeDropdown.id !== event.target.id) {
+    activeDropdown.element.classList.remove('active');
+  }
+  //checking if a list element was clicked, changing the inner button value
+  // if (event.target.tagName === 'LI') {
+  //   activeDropdown.button.innerHTML = event.target.innerHTML;
+  //   for (var i = 0; i < event.target.parentNode.children.length; i++) {
+  //     if (event.target.parentNode.children[i].classList.contains('check')) {
+  //       event.target.parentNode.children[i].classList.remove('check');
+  //     }
+  //   }
+  //   //timeout here so the check is only visible after opening the dropdown again
+  //   window.setTimeout(function () {
+  //     event.target.classList.add('check');
+  //   }, 300)
+  // }
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].classList.contains('dropdown-selection')) {
+      activeDropdown.id = this.id;
+      activeDropdown.element = this.children[i];
+      this.children[i].classList.add('active');
+    }
+    //adding the dropdown-button to our object
+    else if (this.children[i].classList.contains('dropdown-button')) {
+      activeDropdown.button = this.children[i];
+    }
+  }
+}
+
+window.onclick = function (event) {
+  if (!event.target.classList.contains('dropdown-button')) {
+    activeDropdown.element.classList.remove('active');
+  }
+}
+
+
